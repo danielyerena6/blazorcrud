a240a4d9bafb8b1fb110b129b20b5d3c74a8399a
Dan <blasterdaxx2208@gmail.com>
2020-06-26 15:26:44 -0500

    third commit

---
+++ BlazorCrud/BlazorCrud/Data/Firma.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorCrud.Data
+{
+    public class Firma
+    {
+        public object firmar()
+        {
+            System.Diagnostics.Process.Start("Practica 2 Yerena.exe");
+            return "Firma";
+        }
+    }
+}
